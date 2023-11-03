257d77899507df3d1b4176f49af4723f2e2871ca
DaniV93 <124316934+DaniV93@users.noreply.github.com>
2023-11-02 20:25:41 -0500

    Initial commit

---
+++ src/main/java/com/mycompany/tallerdockeraws/SparkWebServer.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.tallerdockeraws;
+
+import static spark.Spark.*;
+
+/**
+ *
+ * @author dverf
+ */
+public class SparkWebServer {
+
+    public static void main(String... args) {
+        port(getPort());
+        get("hello", (req, res) -> "Hello Docker!");
+    }
+
+    private static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;
+    }
+}
